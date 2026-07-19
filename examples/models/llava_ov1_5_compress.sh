85c092068f8154ba72f0def3901e7c011aa086d7
WH-Zhou2003 <zhouweihang2003@163.com>
2026-07-19 15:09:44 +0800

    Update HF_HOME and model arguments in script

--- examples/models/llava_ov1_5_compress.sh
+++ examples/models/llava_ov1_5_compress.sh
@@ -1,9 +1,9 @@
-export HF_HOME="/data_sdc/data/zwh/dataset"
+export HF_HOME="~/.cache/huggingface"
 #video_mmmu/videochatgpt/mvbench/nextqa_mc_test
 # pip install git+https://github.com/EvolvingLMMs-Lab/lmms-eval.git
 export DECORD_EOF_RETRY_MAX=81920
 accelerate launch --num_processes=8 --main_process_port 12399 -m lmms_eval \
     --model=llava_onevision1_5_compress \
-    --model_args=pretrained=lmms-lab/LLaVA-OneVision-1.5-8B-Instruct,attn_implementation=flash_attention_2,max_pixels=3240000 \
+    --model_args=pretrained=lmms-lab/LLaVA-OneVision-1.5-4B-Instruct,attn_implementation=flash_attention_2,max_pixels=3240000 \
     --tasks=videomme \
     --batch_size=1
